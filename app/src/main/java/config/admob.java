287968b4e47fcdcf7025797833e09e74943aae16
im4ddy <contactwithmaddy@gmail.com>
2020-04-07 19:00:32 +0500

    admob

--- app/src/main/java/config/admob.java
+++ app/src/main/java/config/admob.java
@@ -9,9 +9,9 @@ import android.widget.LinearLayout;
 
 public class admob {
 
-    public static String publisherID = "pub-64976752732367751";
-    public static String admBanner = "ca-app-pub-6497675273236775/66877713951";
-	public static String Interstitial = "ca-app-pub-6497675273236775/27485263801";
+    public static String publisherID = "pub-6497675273236775";
+    public static String admBanner = "ca-app-pub-6497675273236775/6687771395";
+	public static String Interstitial = "ca-app-pub-6497675273236775/2748526380";
     public static String privacy_policy_url ="https://sites.google.com/view/xtreemapps/home";
 	
 	public static void admobBannerCall(Activity acitivty , LinearLayout linerlayout){
